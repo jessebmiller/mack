99c7f4a201e45c1281e3e51841e56e4dd479c0bc
Jesse B. Miller <jesse@jessebmiller.com>
2018-02-21 23:48:49 -0600

    refactoring commands

--- dotfiles/.commands/command_not_found_handler.sh
+++
@@ -1,9 +0,0 @@
-command_not_found_handler() {
-    ls "$HOME/.commands/$1.sh"
-    if [ -f "$HOME/.commands/$1.sh" ]; then
-        $(which zsh) "$HOME/.commands/$1.sh" "${@:2}"
-    else
-        echo "$1: command not found"
-        return 127
-    fi
-}
--- mack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#
-# for everything symlinked to this script, run that installer then
-# run it
-#
-
-
