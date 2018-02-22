df2076bdc31ed822c4e577481bf6aa07e3ec23d5
Jesse B. Miller <jesse@jessebmiller.com>
2018-02-21 21:45:56 -0600

    adds command_not_found_handler

--- dotfiles/.commands/chrome.sh
+++ dotfiles/.commands/chrome.sh
@@ -1,13 +1,15 @@
 #! /bin/zsh
-
+echo "$@"
 # Try it
-open "Google Chrome" $@
-if [ $? -ne 0 ]
-then # If it didn't work
+open -a "Google Chrome" "$@"
+
+# If it didn't work
+if [ $? -ne 0 ]; then
+
     # Try to install it
     echo "Installing Google Chrome."
     brew cask install google-chrome
 
     # Then try it again
-    open "Google Chrome"
+    open -a "Google Chrome" "$@"
 fi
