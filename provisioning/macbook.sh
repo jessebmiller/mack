94996b74b85297873d96923ef22ea4d7c995eadb
Jesse B. Miller <jesse@jessebmiller.com>
2018-02-21 23:48:49 -0600

    refactoring commands

--- provisioning/macbook.sh
+++ provisioning/macbook.sh
@@ -2,40 +2,15 @@
 
 set +e
 
-# - homebrew, fonts, chrome, docker, iterm2
-
-echo "Install starting. You may be asked for your password (for sudo)."
-
-# requires xcode and tools!
-xcode-select -p || exit "XCode must be installed! (use the app store)"
-
-# requirements
-cd ~/
-mkdir ~/tmp
-
-# homebrew
-if hash brew &> /dev/null; then
-	echo_ok "Homebrew already installed"
-else
-    echo_warn "Installing homebrew..."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
 # Dock
 echo "fixing the Dock"
 defaults write com.apple.dock tilesize -int 1
 defaults write com.apple.dock autohide-time-modifier -float 20000000000
+defaults write com.apple.Dock autohide -bool TRUE
 killall Dock
 
-brew tap caskroom/cask
-brew tap caskroom/versions
-brew tap caskroom/fonts
-
-brew update
-brew upgrade
-
 # Fonts
-echo "Installing fonts..."
+echo "Installing fonts"
 brew cask install \
   font-anonymous-pro \
   font-dejavu-sans-mono-for-powerline \
@@ -54,28 +29,6 @@ brew cask install \
   font-source-sans-pro \
   font-ubuntu font-ubuntu-mono-powerline
 
-# Apps
-echo "Installing applications..."
-brew cask install \
-     google-chrome \
-     iterm2 \
-     docker \
-     tidal
-
-curl https://prerelease.keybase.io/Keybase.dmg > ~/tmp/Keybase.dmg
-sudo hdiutil attach ~/tmp/Keybase.dmg
-cp -r /Volumes/Keybase/Keybase.app /Applications
-sudo hdiutil detach /Volumes/Keybase
-
 # Install dotfiles
-export GLOBIGNORE="*." # exclude . and .. from glob
-for file in ~/mack/dotfiles/.*; do
-    destinationFile="~/$(basename $file)"
-    ln -s $destinationFile $file
-done
-unset GLOBIGNORE
-
-# configure iterm2
-open "mack/config/SolarizedDark.itermcolors"
-
-rm -rf ~/tmp
+echo "Installing dotfiles"
+/bin/bash ../dotfiles/install.sh
