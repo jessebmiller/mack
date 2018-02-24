f236d04f2cf5609bc69dcfeef137ba9011da35cb
Jesse B. Miller <jesse@jessebmiller.com>
2018-02-24 12:42:33 -0600

    adds dotfiles/.emacs.d

---
+++ dotfiles/.emacs.d/init.el
@@ -0,0 +1,58 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+    (add-to-list 'package-archives (cons "melpa" url) t))
+(add-to-list 'package-archives
+             '("marmalade" . "https://marmalade-repo.org/packages/"))
+
+;; get some ido goodness
+(ido-mode 1)
+(ido-everywhere 1)
+;; in org-mode and magit
+(setq org-completion-use-ido t)
+(setq magit-completing-read-function 'magit-ido-completing-read)
+;; in M-x
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+;; keep smex items in the mounted volume
+(setq smex-save-file "~/.emacs.d/smex-items")
+
+;; disable auto-save
+(setq auto-save-default nil)
+
+;; whitespace and long line highlighting
+(require 'whitespace)
+(setq whitespace-style '(face trailing lines-tail))
+(set-face-attribute 'whitespace-trailing nil
+                    :background "#585858")
+(set-face-attribute 'whitespace-line nil
+                    :background "#585858"
+                    :foreground "#808080")
+(global-whitespace-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (haskell-mode yaml-mode ssh-file-modes smex paredit markdown-mode magit ido-ubiquitous go-mode closure-lint-mode clojurescript-mode cider better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; org mode stuff
+(global-set-key "\C-ca" 'org-agenda)
