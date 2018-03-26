772e9ef688dc7d065f5e72cb87f3123558282804
Jesse B. Miller <jesse@jessebmiller.com>
2018-03-25 21:36:44 -0500

    fix iterm command

--- commands/iterm.sh
+++ commands/iterm.sh
@@ -1,1 +1,16 @@
-brew cask install iterm2
+#! /bin/zsh
+
+# Try it
+open -a iTerm "$@"
+
+# If it didn't work
+if [ $? -ne 0 ]; then
+
+    # Try to install it
+    echo "Installing iterm2"
+    brew cask install iterm2
+
+    # Then try it again
+    open -a iTerm "$@"
+fi
+
